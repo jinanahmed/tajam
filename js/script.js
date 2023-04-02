43186c3b581530de15b2b3323cfd254eddbc8393
rotten-bread <48024572+rotten-bread@users.noreply.github.com>
2023-04-03 00:15:02 +0600

    second

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.testtxtwrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      });
+})
